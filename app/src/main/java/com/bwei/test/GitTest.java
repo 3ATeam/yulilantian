7d5e454047ee22b07c10882c39019d44a29be268
caojun_caojun@126.com <caojun_caojun@126.com>
2017-06-14 11:30:15 +0800

    add GitTest.java

---
+++ app/src/main/java/com/bwei/test/GitTest.java
@@ -0,0 +1,18 @@
+package com.bwei.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by caojun on 2017/6/14.
+ */
+
+public class GitTest extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.e("myMessage","caojun");
+    }
+}
